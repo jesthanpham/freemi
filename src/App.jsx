5944fde6f4bacac8d4663d153391ccfe32a22324
PatLuo <patrick.luong615@gmail.com>
2024-09-28 09:08:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Your Custom Home Page</h1>
+      <p>
+        This is your new Chrome extension home page built with React and Vite!
+      </p>
+      {/* Add more components and functionality here */}
+    </div>
+  );
+}
+
+export default App;
